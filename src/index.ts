c4f08e15ff95bc96be638e086dae212cd46836d3
Alex Howe <mail@ahowe.dev>
2020-02-01 23:09:38 +0000

    Added library files

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      db?: MongoClient;
+    }
+  }
+}
+
+/**
+ * Make an express RequestHandler that sets req.db for your routes
+ * @param connection - The MongoClient you want to pass
+ */
+export default (connection: MongoClient): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!connection) return next(new Error('No connection was passed to express-mongodb.'));
+
+    connection
+      .connect()
+      .then(db => {
+        req.db = db;
+        return next();
+      })
+      .catch(err => {
+        return next(err);
+      });
+  };
+};
